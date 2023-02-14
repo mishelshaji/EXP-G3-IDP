738b4db22c773e0c0481b19647fd737754862a23
Athul Muraleedharan <120544082+athulmuraleedharan@users.noreply.github.com>
2023-02-14 14:18:36 +0530

    unnecessary lines of code had removed.

--- client/idp/src/app/manager/manager-routing.module.ts
+++ client/idp/src/app/manager/manager-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ManagerLayoutComponent } from './manager-layout/manager-layout.component';
 
@@ -9,7 +8,6 @@ const routes: Routes = [
     path: '',
     component: ManagerLayoutComponent,
     children: [
-      { path: 'home', component: HomeComponent },
       { path: 'manager-homepage', component: HomepageComponent },
     ],
   },
