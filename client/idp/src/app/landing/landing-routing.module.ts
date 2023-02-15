c90d6c83b2b55b79af9397e9d3fb3728f5c4f9a9
varsha9999 <varshavmukundh@gmail.com>
2023-02-15 15:01:26 +0530

    error fixed

--- client/idp/src/app/landing/landing-routing.module.ts
+++ client/idp/src/app/landing/landing-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LobbyComponent } from './lobby/lobby.component';
 
 const routes: Routes = [
-  {path:'',component: LobbyComponent}
+  { path: '', component: LobbyComponent }
 ];
 
 @NgModule({
--- client/idp/src/app/landing/landing.module.ts
+++ client/idp/src/app/landing/landing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LandingRoutingModule } from './landing-routing.module';
 import { LobbyComponent } from './lobby/lobby.component';
-
 
 @NgModule({
   declarations: [
