86c2677f815a7b6d34725df613018f543b9167bb
varsha9999 <varshavmukundh@gmail.com>
2023-02-17 10:10:37 +0530

    CHANGED

--- client/idp/src/app/user/user-routing.module.ts
+++ client/idp/src/app/user/user-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ObjectiveDetailedComponent } from '../shared/objective-detailed/objective-detailed.component';
+import { CreateObjectiveComponent } from './create-objective/create-objective.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ShowAllIdpComponent } from './show-all-idp/show-all-idp.component';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
     path: '', component: UserLayoutComponent, children: [
       { path: 'home', component: HomepageComponent },
       { path: 'objective-detailed', component: ObjectiveDetailedComponent },
-      { path: 'idp', component: ShowAllIdpComponent }
+      { path: 'idp', component: ShowAllIdpComponent },
+      { path: 'objective', component: CreateObjectiveComponent }
     ]
   }
 ];
