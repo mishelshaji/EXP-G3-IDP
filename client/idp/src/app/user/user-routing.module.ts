3159dca041de3adb9ea292ce042eabaaac7aee63
Mahesh P <pmahesh072002@gmail.com>
2023-02-19 17:41:17 +0530

    Update user-routing.module.ts

--- client/idp/src/app/user/user-routing.module.ts
+++ client/idp/src/app/user/user-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddActionComponent } from './add-action/add-action.component';
 import { AddTrainingComponent } from './add-training/add-training.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { ObjectiveDetailedComponent } from '../shared/objective-detailed/objective-detailed.component';
 import { ObjectivesComponent } from '../shared/objectives/objectives.component';
 import { ProfileComponent } from './profile/profile.component';
 import { CreateObjectiveComponent } from './create-objective/create-objective.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ObjectiveDetailedComponent } from '../shared/objective-detailed/objective-detailed.component';
+import { SigninComponent } from './signin/signin.component';
 import { ShowAllIdpComponent } from './show-all-idp/show-all-idp.component';
 import { UserLayoutComponent } from './user-layout/user-layout.component';
-import { UserUploadComponent } from './user-upload/user-upload.component';
 
 const routes: Routes = [
   {
@@ -22,9 +23,9 @@ const routes: Routes = [
       { path: 'add-training', component: AddTrainingComponent },
       { path: 'add-action', component: AddActionComponent},
       { path: 'home', component: HomepageComponent },
+      { path: 'objective-detailed', component: ObjectiveDetailedComponent },
       { path: 'idp', component: ShowAllIdpComponent },
-      { path: 'create-objective', component: CreateObjectiveComponent },
-      { path: 'userupload', component: UserUploadComponent }
+      { path: 'objective', component: CreateObjectiveComponent }
     ]
   }
 ];
